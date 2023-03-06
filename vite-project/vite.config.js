755023e62b4b99e9a8d513d1cb15a06f8494eaad
sal <salgarcia940@gmail.com>
2023-03-05 20:08:18 -0800

    new commit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/erics-mobile-welding/',
+  base: '/erics-mobile-welding',
   plugins: [react()],
 })
